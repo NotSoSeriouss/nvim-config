6bed187fbe2ace473885207f864d5f297f8e6bc9
pesce <superplayer292@gmail.com>
2024-01-05 11:44:39 +0000

    Codeium

--- lua/pollo/remaps.lua
+++ lua/pollo/remaps.lua
@@ -15,3 +15,6 @@ vim.keymap.set("n", "<leader>lc", "<cmd>Lazy clean<cr>")
 vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>pg", "<cmd>Telescope grep_string<cr>")
 
+-- Codeium
+vim.keymap.set("i", "<S-Right>", function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+
