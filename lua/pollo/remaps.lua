4e5e0c28eb964d995acc52cf1eb3a8523bd79ef7
pesce <superplayer292@gmail.com>
2024-01-05 10:52:32 +0000

    settings, remaps, formatting

--- lua/pollo/remaps.lua
+++ lua/pollo/remaps.lua
@@ -1,6 +1,7 @@
 -- General
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Rex)
+vim.keymap.set("n", "<leader>f", "gg^VG=")
 
 -- Lazy
 vim.keymap.set("n", "<leader>lh", "<cmd>Lazy home<cr>")
