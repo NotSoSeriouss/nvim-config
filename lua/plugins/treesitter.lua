286bebb5f6b5f5ada7fe7e960b8a00ace6f13332
Michele Pollidori <superplayer292@gmail.com>
2024-01-05 11:19:29 +0100

    fixes +ts fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,42 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	config = function()
-		require('nvim-treesitter.configs').setup({
-			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+				{ "JoosepAlviste/nvim-ts-context-commentstring"},
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				-- HACK: remove when https://github.com/windwp/nvim-ts-autotag/issues/125 closed.
+				{ "windwp/nvim-ts-autotag", opts = { enable_close_on_slash = false } },
+		},
+		cmd = {
+				"TSBufDisable",
+				"TSBufEnable",
+				"TSBufToggle",
+				"TSDisable",
+				"TSEnable",
+				"TSToggle",
+				"TSInstall",
+				"TSInstallInfo",
+				"TSInstallSync",
+				"TSModuleInfo",
+				"TSUninstall",
+				"TSUpdate",
+				"TSUpdateSync",
+		},
+		build = ":TSUpdate",
+		init = function(plugin)
+				require("lazy.core.loader").add_to_rtp(plugin)
+				require "nvim-treesitter.query_predicates"
+		end,
+		config = function()
+				require'nvim-treesitter.configs'.setup {
+						-- A list of parser names, or "all" (the five listed parsers should always be installed)
+						ensure_installed = {"lua", "vim", "vimdoc"},
+
+						-- Install parsers synchronously (only applied to `ensure_installed`)
+						sync_install = false,
+						highlight = {
+								enable = true,
+								additional_vim_regex_highlighting = false,
+						},
+				}
+		end,
 }
