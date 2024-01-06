d989b5f14b7496675d96291c41503cac0c49645c
root <superplayer292@gmail.com>
2024-01-06 10:57:16 +0000

    VS theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -26,5 +26,40 @@ local dracula = {
 	}
 }
 
+local vscode = {
+	"Mofiqul/vscode.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
 
-return dracula
+		vim.o.background = 'dark'
+
+		local c = require('vscode.colors').get_colors()
+		require('vscode').setup({
+			-- Alternatively set style in setup
+			-- style = 'light'
+
+			-- Enable transparent background
+			transparent = true,
+
+			-- Enable italic comment
+			italic_comments = true,
+
+			-- Disable nvim-tree background color
+			disable_nvimtree_bg = false,
+
+			-- Override highlight groups (see ./lua/vscode/theme.lua)
+			group_overrides = {
+				-- this supports the same val table as vim.api.nvim_set_hl
+				-- use colors from this colorscheme by requiring vscode.colors!
+				Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+			}
+		})
+		require('vscode').load()
+	end,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	}
+}
+
+return vscode
