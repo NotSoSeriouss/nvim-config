7f9ca7e7d65340c4d018a2cb2ef32ab92faa5d48
NotSoSeriouss <77798806+NotSoSeriouss@users.noreply.github.com>
2024-02-09 13:48:50 +0100

    v2.1 (#10)
    * NO CODEIUM BECAUSE 250MB OF RAM
    
    * I Forgor
    
    * Default theme
    
    * Small Improvements
    
    ---------
    
    Co-authored-by: Michele Pollidori <pesce>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,8 +14,6 @@ return {
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(_, bufnr)
-				-- see :help lsp-zero-keybindings
-				-- to learn the available actions
 				lsp_zero.default_keymaps({buffer = bufnr})
 			end)
 
