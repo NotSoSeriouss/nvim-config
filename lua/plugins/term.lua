e6cfa891a2ae3051dcad90ae6ab026414be3e8b9
root <superplayer292@gmail.com>
2024-02-12 16:38:52 +0000

    Windows ps support

--- lua/plugins/term.lua
+++ lua/plugins/term.lua
@@ -5,6 +5,10 @@ return {
 		config = function()
 			require("toggleterm").setup{
 				shell = function()
+					if os.getenv("HOME") == nil then
+						return "%SystemRoot%\\system32\\WindowsPowerShell\\v1.0\\powershell.exe"
+					end
+
 					local ok = os.execute("find /bin/zsh | grep .")
 					if ok then
 						return "/bin/zsh"
